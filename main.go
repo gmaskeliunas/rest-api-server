431104f4f6eaf1d88742ecf542e33d67caf067fc
Gediminas Maskeliunas <ged.mask@gmail.com>
2024-07-02 16:42:42 +0300

    added Rest APIs

--- main.go
+++ main.go
@@ -10,11 +10,24 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello world")
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
 	})
 
-	if err := http.ListenAndServe("localhost:8080", mux); err != nil {
+	mux.HandleFunc("GET /comment/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		fmt.Fprintf(w, "return a single comment for comment with id: %s", id)
+	})
+
+	mux.HandleFunc("GET /comment", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "return all comments")
+	})
+
+	mux.HandleFunc("POST /comment", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "post a new comment")
+	})
+
+	if err := http.ListenAndServe("localhost:8000", mux); err != nil {
 		fmt.Println(err.Error())
 	}
 }
